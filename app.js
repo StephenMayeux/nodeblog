b49e4ba02715ac336d9f90d3c187a85ea8c540f9
Stephen Mayeux <stephenmayeux@gmail.com>
2015-11-22 16:06:31 +0000

    fix flash messages

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 
 // Connect-Flash
-app.use(flash());
+app.use(require('connect-flash')());
 app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
   next();
