0d265a8a8572d692d1298c480adc6b83aca54d0f
Stephen Mayeux <stephenmayeux@gmail.com>
2015-11-22 16:37:55 +0000

    format index view and styles

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require("mongodb");
+var db = require("monk")("localhost/nodeblog");
 
-/* GET home page. */
+// HOme page blog posts
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db = req.db;
+  var posts = db.get('posts'); //name of the collection we're getting
+  posts.find({},{},function(err, posts) { // empty curlies bc we want all posts
+      if (err) throw err;
+      res.render('index',{
+          "posts": posts
+      });
+  });
 });
 
 module.exports = router;
