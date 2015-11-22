734ec79d8c637176a93d76acf74f54252877339a
Stephen Mayeux <stephenmayeux@gmail.com>
2015-11-22 15:59:25 +0000

    layout and styles

--- app.js
+++ app.js
@@ -18,6 +18,20 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+
+// Connect-Flash
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
 app.set('view engine', 'jade');
 
 // Handle File Uploads & Multipart Data
@@ -29,6 +43,32 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Express Session
+app.use(session({
+    secret: 'secret', // change this for production
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Express Validator -- makes errors nice and formatted
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
