10aad21d4ed0857bfafd27cc1febca6e30cd8d52
Stephen Mayeux <stephenmayeux@gmail.com>
2015-12-05 12:25:59 +0000

    add favicon

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.set('view engine', 'jade');
 app.use(multer({ dest: './public/images/uploads'}));
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
