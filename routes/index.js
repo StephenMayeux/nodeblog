2cf714820e6c03beda108acb8ba6750715c8b0d2
Stephen Mayeux <stephenmayeux@gmail.com>
2015-12-14 07:54:44 +0000

    add URI

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongo = require("mongodb");
-var db = require("monk")("localhost/nodeblog");
+var db = require("monk")(process.env.MONGOLAB_URI);
 
 // HOme page blog posts
 router.get('/', function(req, res, next) {
